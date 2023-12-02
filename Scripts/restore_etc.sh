4ddadeb0e336c733ee64f4fc9ccb36e400b97f5d
topazrn <40129033+topazrn@users.noreply.github.com>
2023-12-02 23:04:20 +0700

    Grub theme still doesn't work

--- Scripts/restore_etc.sh
+++ Scripts/restore_etc.sh
@@ -37,34 +37,34 @@ else
 fi
 
 
-# grub
-if pkg_installed grub2-common
-    then
+# grub WIP
+# if pkg_installed grub2-common
+#     then
 
-    if [ ! -f /etc/default/grub.t2.bkp ] && [ ! -f /boot/grub2/grub.t2.bkp ]
-        then
-        echo "configuring grub..."
-        sudo cp /etc/default/grub /etc/default/grub.t2.bkp
-        sudo mkdir /usr/share/grub/themes/
-        sudo tar -xzf ${CloneDir}/Source/arcs/Grub_Pochita.tar.gz -C /usr/share/grub/themes/
+#     if [ ! -f /etc/default/grub.t2.bkp ] && [ ! -f /boot/grub2/grub.t2.bkp ]
+#         then
+#         echo "configuring grub..."
+#         sudo cp /etc/default/grub /etc/default/grub.t2.bkp
+#         sudo mkdir /usr/share/grub/themes/
+#         sudo tar -xzf ${CloneDir}/Source/arcs/Grub_Pochita.tar.gz -C /usr/share/grub/themes/
 
-        if nvidia_detect
-            then
-            sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet splash nvidia_drm.modeset=1\"" /etc/default/grub
-        fi
+#         if nvidia_detect
+#             then
+#             sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet splash nvidia_drm.modeset=1\"" /etc/default/grub
+#         fi
 
-        sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
-        /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1280x1024x32,auto
-        /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/pochita/theme.txt\"
-        /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" /etc/default/grub
+#         sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
+#         /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1280x1024x32,auto
+#         /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/pochita/theme.txt\"
+#         /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" /etc/default/grub
 
-        sudo cp /boot/grub2/grub.cfg /boot/grub2/grub.t2.bkp
-        sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-    fi
+#         sudo cp /boot/grub2/grub.cfg /boot/grub2/grub.t2.bkp
+#         sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+#     fi
 
-else
-    echo "WARNING: grub is not installed..."
-fi
+# else
+#     echo "WARNING: grub is not installed..."
+# fi
 
 
 # dolphin
