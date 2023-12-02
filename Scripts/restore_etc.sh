9218d1c3eac67175423fb89962db7e99e178124d
topazrn <40129033+topazrn@users.noreply.github.com>
2023-12-03 00:36:31 +0700

    Changing shell stops installation

--- Scripts/restore_etc.sh
+++ Scripts/restore_etc.sh
@@ -75,17 +75,3 @@ else
     echo "WARNING: dolphin is not installed..."
 fi
 
-
-# zsh
-if pkg_installed zsh
-    then
-
-    if [ "$SHELL" != "/usr/bin/zsh" ] ; then
-        echo "changing shell to zsh..."
-        chsh -s $(which zsh)
-    fi
-
-else
-    echo "WARNING: zsh is not installed..."
-fi
-
