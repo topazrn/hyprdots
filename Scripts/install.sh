e4ac8abd3ce80d02777c426a5f4a86b1b1d54315
Straight <107145976+DaEpicR@users.noreply.github.com>
2023-12-10 07:05:46 +0000

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,63 +4,9 @@
 #|-/ /--| Prasanth Rangan          |-/ /--|#
 #|/ /---+--------------------------+/ /---|#
 
-cat <<"EOF"
-
------------------------------------------------------------------
-        .                                                     
-       / \         _       _  _                  _     _      
-      /^  \      _| |_    | || |_  _ _ __ _ _ __| |___| |_ ___
-     /  _  \    |_   _|   | __ | || | '_ \ '_/ _` / _ \  _(_-<
-    /  | | ~\     |_|     |_||_|\_, | .__/_| \__,_\___/\__/__/
-   /.-'   '-.\                  |__/|_|                       
-
------------------------------------------------------------------
-
-EOF
-
-#--------------------------------#
-# import variables and functions #
-#--------------------------------#
-source global_fn.sh
-if [ $? -ne 0 ]; then
-    echo "Error: unable to source global_fn.sh, please execute from $(dirname "$(realpath "$0")")..."
-    exit 1
-fi
-
-#------------------#
-# evaluate options #
-#------------------#
-flg_Install=0
-flg_Restore=0
-flg_Service=0
-
-while getopts idrs RunStep; do
-    case $RunStep in
-    i)  flg_Install=1 ;;
-    d)  flg_Install=1
-        export use_default="-y" ;;
-    r)  flg_Restore=1 ;;
-    s)  flg_Service=1 ;;
-    *)  echo "...valid options are..."
-        echo "i : [i]nstall hyprland without configs"
-        echo "d : install hyprland [d]efaults without configs --noconfirm"
-        echo "r : [r]estore config files"
-        echo "s : enable system [s]ervices"
-        exit 1 ;;
-    esac
-done
-
-if [ $OPTIND -eq 1 ]; then
-    flg_Install=1
-    flg_Restore=1
-    flg_Service=1
-fi
-
-#------------#
-# installing #
-#------------#
-if [ $flg_Install -eq 1 ]; then
-    cat <<"EOF"
+# Function to install packages
+install_packages() {
+    cat <<EOF
 
  _         _       _ _ _         
 |_|___ ___| |_ ___| | |_|___ ___ 
@@ -70,20 +16,16 @@ if [ $flg_Install -eq 1 ]; then
 
 EOF
 
-    #----------------------#
-    # prepare package list #
-    #----------------------#
+    # Prepare package list
     shift $((OPTIND - 1))
     cust_pkg=$1
     cp custom_hypr.lst install_pkg.lst
 
     if [ -f "$cust_pkg" ] && [ ! -z "$cust_pkg" ]; then
-        cat $cust_pkg >>install_pkg.lst
+        cat "$cust_pkg" >>install_pkg.lst
     fi
 
-    #--------------------------------#
-    # add nvidia drivers to the list #
-    #--------------------------------#
+    # Add Nvidia drivers to the list
     if nvidia_detect; then
         cat /usr/lib/modules/*/pkgbase | while read krnl; do
             echo "${krnl}-headers" >>install_pkg.lst
@@ -93,22 +35,17 @@ EOF
             awk -F '|' -v nvc="${nvcode}" '{if ($3 == nvc) {split(FILENAME,driver,"/"); print driver[length(driver)],"\nnvidia-utils"}}' .nvidia/nvidia*dkms >>install_pkg.lst
         done
     else
-        echo "nvidia card not detected, skipping nvidia drivers..."
+        echo "Nvidia card not detected, skipping Nvidia drivers..."
     fi
 
-    #--------------------------------#
-    # install packages from the list #
-    #--------------------------------#
+    # Install packages from the list
     ./install_pkg.sh install_pkg.lst
     rm install_pkg.lst
+}
 
-fi
-
-#---------------------------#
-# restore my custom configs #
-#---------------------------#
-if [ $flg_Restore -eq 1 ]; then
-    cat <<"EOF"
+# Function to restore configurations
+restore_configs() {
+    cat <<EOF
 
              _           _         
  ___ ___ ___| |_ ___ ___|_|___ ___ 
@@ -120,20 +57,11 @@ EOF
 
     ./restore_fnt.sh
     ./restore_cfg.sh
-fi
+}
 
-#---------------------------#
-# update sddm, grub and zsh #
-#---------------------------#
-if [ $flg_Install -eq 1 ] && [ $flg_Restore -eq 1 ]; then
-    ./restore_etc.sh
-fi
-
-#------------------------#
-# enable system services #
-#------------------------#
-if [ $flg_Service -eq 1 ]; then
-    cat <<"EOF"
+# Function to enable system services
+enable_services() {
+    cat <<EOF
 
                  _             
  ___ ___ ___ _ _|_|___ ___ ___ 
@@ -143,6 +71,77 @@ if [ $flg_Service -eq 1 ]; then
 EOF
 
     while read service ; do
-        service_ctl $service
+        service_ctl "$service"
     done < system_ctl.lst
+}
+
+# Main script
+cat <<EOF
+
+-----------------------------------------------------------------
+ _     _                      _                             
+(_)   (_)                    | |       _             
+ _______ _   _ ____   ____ __| | ___ _| |_  ___    
+|  ___  | | | |  _ \ / ___) _  |/ _ (_   _)/___)  
+| |   | | |_| | |_| | |  ( (_| | |_| || |_|___ |   
+|_|   |_|\__  |  __/|_|   \____|\___/  \__|___/            
+        (____/|_|                                               
+                       _   
+                     _| |_ 
+                    (_   _)
+                      |_|  
+                 ..........     
+              ........-+##+...  
+             .......-###+++--.. 
+            ........+#+....---..
+            ........+#+....---..
+            ...--+#######----...
+            ..-----+###---......
+            .---....+#+.........
+            .---...-##-........ 
+            ..--#####-........  
+            ....----.......            
+-----------------------------------------------------------------
+
+EOF
+
+# Import variables and functions
+source global_fn.sh
+
+# Evaluate options
+flg_Install=0
+flg_Restore=0
+flg_Service=0
+
+while getopts "idrs" RunStep; do
+    case $RunStep in
+        i) flg_Install=1 ;;
+        d) flg_Install=1; use_default="-y" ;;
+        r) flg_Restore=1 ;;
+        s) flg_Service=1 ;;
+        *) echo "Valid options are: i, d, r, s"; exit 1 ;;
+    esac
+done
+
+if [ $OPTIND -eq 1 ]; then
+    flg_Install=1
+    flg_Restore=1
+    flg_Service=1
+fi
+
+# Perform actions based on options
+if [ $flg_Install -eq 1 ]; then
+    install_packages
+fi
+
+if [ $flg_Restore -eq 1 ]; then
+    restore_configs
+fi
+
+if [ $flg_Install -eq 1 ] && [ $flg_Restore -eq 1 ]; then
+    ./restore_etc.sh
+fi
+
+if [ $flg_Service -eq 1 ]; then
+    enable_services
 fi
